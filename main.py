e6706dba850b37ea35ecf8b8b81d5839ddd9a999
Hari <baluoger91@gmail.com>
2021-06-26 16:37:31 +0530

    Connection error fixed

--- main.py
+++ main.py
@@ -2,25 +2,29 @@ import requests
 from datetime import date
 
 def Main():
-	param = {
+	try:
+		param = {
 		"country" : "in",
 		"sortBy" : "top",
 		"apiKey" : "67079904a0ab4ac8b188b7f432a99569"
-	}
-	url = "https://newsapi.org/v2/top-headlines"
-	res = requests.get(url,params = param)
-	#print(res)
-	page = res.json()
-	articles = page["articles"]
-	results = []
+		}
+		url = "https://newsapi.org/v2/top-headlines"
+		res = requests.get(url,params = param)
+		#print(res)
+		page = res.json()
+		articles = page["articles"]
+		title = []
 
-	for ar in articles:
-		results.append(ar["title"])
+		for ar in articles:
+			title.append(ar["title"])
 
-	print(date.today())
+		print(date.today())
 
-	for i in range(len(results)):
-		print(i+1,results[i])
+		for i in range(len(title)):
+			print(i+1,title[i])
+
+	except Exception:
+		print("Check Your Internet Connection...!")
 
 if __name__ == '__main__':
 	Main()
